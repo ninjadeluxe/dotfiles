67d6f44067575938e71b00bbcb27dc4b90ae3591
ninjadeluxe <rzleonhardt@gmail.com>
2024-01-08 14:30:26 +0100

    Polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		MONITOR=$mon polybar &
+	done
+}
+
+launch_bar
